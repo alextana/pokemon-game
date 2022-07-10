a7a10cbc3a3cacde5d7a004081b23f656b041415
Alex Tana <alextana@Alexs-MacBook-Air.local>
2022-07-10 20:27:22 +0100

    add access denied screen

--- components/auth/AccessDenied.tsx
+++ components/auth/AccessDenied.tsx
@@ -4,18 +4,22 @@ import Link from 'next/link'
 export default function AccessDenied() {
   return (
     <>
-      <h1>Access Denied</h1>
-      <p>
-        <Link
-          href='/api/auth/signin'
-          onClick={(e) => {
-            e.preventDefault()
-            signIn()
-          }}
-        >
-          You must be signed in to view this page
-        </Link>
-      </p>
+      <div className='w-screen h-screen overflow-hidden bg-gradient-to-t from-green-600 to-green-400'>
+        <div className='pokemon-container top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute mx-auto w-max text-center'>
+          <h1 className='text-4xl font-bold tracking-tighter'>Access Denied</h1>
+          <p>
+            <Link
+              href='/api/auth/signin'
+              onClick={(e) => {
+                e.preventDefault()
+                signIn()
+              }}
+            >
+              You must be signed in to view this page
+            </Link>
+          </p>
+        </div>
+      </div>
     </>
   )
 }
